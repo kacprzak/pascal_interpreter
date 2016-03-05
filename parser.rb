ad5b0fb8b741c26880c8389496ef394116afec0c
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-03-05 13:18:40 +0100

    moved parser to separate file

---
+++ parser.rb
@@ -0,0 +1,98 @@
+require_relative 'lexer'
+
+class AST
+end
+
+
+class BinOp < AST
+  attr_reader :left, :token, :right
+  alias_method :op, :token
+
+  def initialize(left, token, right)
+    @left = left
+    @token = token
+    @right = right
+  end
+end
+
+
+class Num < AST
+  attr_reader :token
+  
+  def initialize(token)
+    @token = token
+  end
+
+  def value
+    @token.value
+  end
+end
+
+
+class Parser
+  def initialize(lexer)
+    @lexer = lexer
+    @current_token = @lexer.get_next_token
+  end
+
+  def parse
+    expr
+  end
+  
+  private
+  def error
+    raise 'Invalid syntax'
+  end
+  
+  def eat(token_type)
+    if @current_token.type == token_type
+      @current_token = @lexer.get_next_token
+    else
+      error
+    end
+  end
+
+  # factor : INTEGER | LPAREN expr RPAREN
+  def factor
+    token = @current_token
+    if token.type == :integer
+      eat :integer
+      Num.new(token)
+    elsif token.type == :lparen
+      eat :lparen
+      node = expr
+      eat :rparen
+      node
+    end
+  end
+  
+  # term: factor ((MUL | DIV) factor)*
+  def term
+    node = factor
+    while [:mul, :div].include? @current_token.type
+      token = @current_token
+      if token.type == :mul
+        eat :mul
+      elsif token.type == :div
+        eat :div
+      end
+      node = BinOp.new(node, token, factor)
+    end
+    node
+  end
+
+  # expr: term ((PLUS | MINUS) term)*
+  def expr
+    node = term
+    while [:plus, :minus].include? @current_token.type
+      token = @current_token
+      if token.type == :plus
+        eat :plus
+      elsif token.type == :minus
+        eat :minus
+      end
+      node = BinOp.new(node, token, term)
+    end
+    node
+  end
+end
--- pascal_interpreter.rb
+++ pascal_interpreter.rb
@@ -1,105 +1,5 @@
 #!/usr/bin/ruby
-require_relative 'lexer'
-
-class AST
-end
-
-
-class BinOp < AST
-  attr_reader :left, :token, :right
-  alias_method :op, :token
-
-  def initialize(left, token, right)
-    @left = left
-    @token = token
-    @right = right
-  end
-end
-
-
-class Num < AST
-  attr_reader :token
-  
-  def initialize(token)
-    @token = token
-  end
-
-  def value
-    @token.value
-  end
-end
-
-
-class Parser
-  def initialize(lexer)
-    @lexer = lexer
-    @current_token = @lexer.get_next_token
-  end
-
-  def parse
-    expr
-  end
-  
-  private
-  def error
-    raise 'Invalid syntax'
-  end
-  
-  def eat(token_type)
-    if @current_token.type == token_type
-      @current_token = @lexer.get_next_token
-    else
-      error
-    end
-  end
-
-  # factor : INTEGER | LPAREN expr RPAREN
-  def factor
-    token = @current_token
-    if token.type == :integer
-      eat :integer
-      token.value
-    elsif token.type == :lparen
-      eat(:lparen)
-      result = expr
-      eat(:rparen)
-      result
-    end
-  end
-  
-  # term: factor ((MUL | DIV) factor)*
-  def term
-    result = factor
-    while [:mul, :div].include? @current_token.type
-      token = @current_token
-      if token.type == :mul
-        eat :mul
-        result = result * factor
-      elsif token.type == :div
-        eat :div
-        result = result / factor
-      end
-    end
-    result
-  end
-
-  # expr: term ((PLUS | MINUS) term)*
-  def expr
-    result = term
-    while [:plus, :minus].include? @current_token.type
-      token = @current_token
-      if token.type == :plus
-        eat :plus
-        result = result + term
-      elsif token.type == :minus
-        eat :minus
-        result = result - term
-      end
-    end
-    result
-  end
-end
-
+require_relative 'parser'
 
 def main
   loop do
@@ -108,8 +8,8 @@ def main
     text = gets.chomp
     next if text.empty?
     lexer = Lexer.new(text)
-    interpreter = Parser.new(lexer)
-    puts interpreter.parse
+    parser = Parser.new(lexer)
+    puts parser.parse
   end
 end
 
