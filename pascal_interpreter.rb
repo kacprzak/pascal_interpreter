06ed6dedda12f4950ea0f31d8fca2372e4bd1c99
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-03-05 13:43:27 +0100

    Interpreter class

--- pascal_interpreter.rb
+++ pascal_interpreter.rb
@@ -1,6 +1,36 @@
 #!/usr/bin/ruby
 require_relative 'parser'
 
+class NodeVisitor
+  def visit(node)
+    method_name = "visit_#{node.class.name}"
+    send(method_name, node)
+  end
+end
+
+
+class Interpreter < NodeVisitor
+  def initialize(parser)
+    @parser = parser
+  end
+
+  def interpret
+    visit(@parser.parse)
+  end
+
+  private
+  def visit_BinOp(node)
+    left_value = visit(node.left)
+    right_value = visit(node.right)
+    left_value.public_send(node.op.value, right_value)
+  end
+  
+  def visit_Num(node)
+    node.value
+  end
+end
+
+
 def main
   loop do
     print 'calc> '
@@ -9,7 +39,8 @@ def main
     next if text.empty?
     lexer = Lexer.new(text)
     parser = Parser.new(lexer)
-    puts parser.parse
+    interpreter = Interpreter.new(parser)
+    puts interpreter.interpret
   end
 end
 
