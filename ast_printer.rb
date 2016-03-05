98633961b6d8840354c16f4a1166fa9ff5979575
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-03-05 15:56:24 +0100

    ast_printer refactored

--- ast_printer.rb
+++ ast_printer.rb
@@ -4,6 +4,11 @@ require_relative 'parser'
 require_relative 'node_visitor'
 
 class ASTPrinter < NodeVisitor
+  VERTICAL = '│'
+  HORIZONTAL = '─'
+  VERTICAL_AND_RIGHT = '├'
+  UP_AND_RIGHT = '└' 
+  
   def initialize(parser)
     @parser = parser
     @indent = ""
@@ -23,18 +28,18 @@ class ASTPrinter < NodeVisitor
   end
 
   def puts_node(node_value)
-    puts @indent + "─(" + node_value + ")"
+    puts @indent + HORIZONTAL + "(" + node_value + ")"
 
-    @indent[-1,1] = "│" if @indent[-1, 1] == "├"
-    @indent[-1,1] = " " if @indent[-1, 1] == "└"
+    @indent[-1,1] = VERTICAL if @indent[-1,1] == VERTICAL_AND_RIGHT
+    @indent[-1,1] = " " if @indent[-1,1] == UP_AND_RIGHT
   end
 
   def visit_BinOp(node)
     puts_node node.op.value
-    increase_indent "  ├"
+    increase_indent("  " + VERTICAL_AND_RIGHT)
     visit(node.left)
     decrease_indent 3
-    increase_indent "  └"
+    increase_indent("  " + UP_AND_RIGHT)
     visit(node.right)
     decrease_indent 3
   end
